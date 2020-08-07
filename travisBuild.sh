c3719ec0ecdb5fd955aaa11aba21e89eea86760c
agorararmard <aagouhar@efabless.com>
2020-08-07 17:30:52 +0200

    relocate build into make

--- travisBuild.sh
+++ travisBuild.sh
@@ -2,4 +2,5 @@ make openlane
 mkdir pdks
 export PDK_ROOT=$(pwd)/pdks
 export RUN_ROOT=$(pwd)
-docker run -it -v $RUN_ROOT:/openLANE_flow -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT openlane:rc3  bash -c "make clone-skywater-pdk"
+docker run -it -v $RUN_ROOT:/openLANE_flow -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT openlane:rc3  bash -c "make pdk"
+exit 0
