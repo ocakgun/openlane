17f8977ef6d548e176c7db035c8038ea0a954fa6
agorararmard <aagouhar@efabless.com>
2020-08-07 23:01:34 +0200

    test sets

---
+++ travisCI/travisBuild.sh
@@ -0,0 +1,30 @@
+make openlane
+mkdir pdks
+export PDK_ROOT=$(pwd)/pdks
+export RUN_ROOT=$(pwd)
+echo $PDK_ROOT
+echo $RUN_ROOT
+cd  $PDK_ROOT
+rm -rf skywater-pdk
+git clone https://github.com/google/skywater-pdk.git skywater-pdk
+echo "cloned skywater-pdk"
+cd skywater-pdk
+echo "went into skywater-pdk"
+cd  $PDK_ROOT/skywater-pdk
+echo "went into skywater-pdk"
+git submodule update --init libraries/sky130_fd_sc_hd/latest
+echo "got submodule"
+make sky130_fd_sc_hd
+echo "ran make on sky130_fd_sc_hd" 
+cd $PDK_ROOT 
+rm -rf open_pdks 
+git clone https://github.com/RTimothyEdwards/open_pdks.git open_pdks
+echo "cloned open_pdks"
+cd open_pdks 
+echo "went into open_pdks"
+git checkout -qf 60b4f62aabff2e4fd9df194b6db59e61a2bd2472
+echo "checkedout open_pdks"
+docker run -it -v $RUN_ROOT:/openLANE_flow -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT openlane:rc3  bash -c "make install-open_pdks"
+echo "done installing"
+cd $RUN_ROOT
+exit 0
--- travisTest.sh
+++
@@ -1,5 +0,0 @@
-design=$1
-export PDK_ROOT=$(pwd)/pdks
-export RUN_ROOT=$(pwd)
-docker run -it -v $RUN_ROOT:/openLANE_flow -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT openlane:rc3  bash -c "python3 run_designs.py -d $design -t TEST_$design -b regression_results/benchmark_results/SW_HD.csv"
-grep "PASSED" $RUN_ROOT/regression_results/TEST_$design_*/TEST_$design_*_design_test_report.csv
