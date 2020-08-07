abd880f1972f99e679c08f21190ae5395e2851b7
agorararmard <aagouhar@efabless.com>
2020-08-07 17:16:39 +0200

    added debug messages

--- travisBuild.sh
+++ travisBuild.sh
@@ -1,9 +1,14 @@
 make openlane
 mkdir pdks
 export PDK_ROOT=$(pwd)/pdks
-cd  $(PDK_ROOT)
+export RUN_ROOT=$(pwd)
+cd  $PDK_ROOT
 rm -rf skywater-pdk
 git clone --depth 30 https://github.com/google/skywater-pdk.git skywater-pdk
+echo "Done creating skywater-pdks"
 cd skywater-pdk
-docker run -it -v $(pwd):/openLANE_flow -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT openlane:rc3  bash -c "make skywater-library"
-docker run -it -v $(pwd):/openLANE_flow -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT openlane:rc3  bash -c "make open_pdks"
+echo "cd inside skywater-pdk"
+echo $RUN_ROOT
+echo $PDK_ROOT
+docker run -it -v $RUN_ROOT:/openLANE_flow -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT openlane:rc3  bash -c "make skywater-library"
+docker run -it -v $RUN_ROOT:/openLANE_flow -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT openlane:rc3  bash -c "make open_pdks"
